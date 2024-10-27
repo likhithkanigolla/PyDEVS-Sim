d297b467b052b80c5220ec83556903e270c0dcd5
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-10-27 21:16:53 +0530

    Refactor code for water quality sensing experiment

--- basic-model/onem2m_interface.py
+++ basic-model/onem2m_interface.py
@@ -1,38 +1,37 @@
 from pypdevs.DEVS import AtomicDEVS
 from pypdevs.infinity import INFINITY
-import random
 
-class OneM2MInterfaceState:
+class WaterQualitySensorState:
     def __init__(self):
-        self.processing_time = 0.0 
+        self.next_reading_time = 0.0
         self.data_to_send = None
 
-class OneM2MInterface(AtomicDEVS):
-    def __init__(self, server_url, username, password, simulated_delay=1.0):
-        AtomicDEVS.__init__(self, "OneM2MInterface")
-        self.server_url = server_url
-        self.auth = (username, password)
-        self.simulated_delay = simulated_delay
-        self.state = OneM2MInterfaceState()
+class WaterQualitySensor(AtomicDEVS):
+    def __init__(self, reading_interval=1.0):
+        AtomicDEVS.__init__(self, "WaterQualitySensor")
+        self.reading_interval = reading_interval
+        self.state = WaterQualitySensorState()
+        self.timeLast = 0.0  # Initialize timeLast
         self.inport = self.addInPort("in")
         self.outport = self.addOutPort("out")
 
     def timeAdvance(self):
         if self.state.data_to_send is None:
-            return INFINITY 
-        return self.state.processing_time - self.timeLast
+            return INFINITY
+        return self.state.next_reading_time - self.timeLast
 
     def extTransition(self, inputs):
         self.state.data_to_send = inputs[self.inport]
-        self.state.processing_time = self.timeLast + self.simulated_delay
+        self.state.next_reading_time = self.timeLast + self.reading_interval
         return self.state
 
     def outputFnc(self):
         sent_data = self.state.data_to_send
         self.state.data_to_send = None
-        print(f"OneM2M Interface simulated sending: {sent_data}")
+        print(f"Water Quality Sensor simulated sending: {sent_data}")
         return {self.outport: sent_data}
 
     def intTransition(self):
-        self.state.processing_time = INFINITY 
+        self.timeLast = self.state.next_reading_time  # Update timeLast
+        self.state.next_reading_time = INFINITY
         return self.state
