c4c8c0cbda923baeee65f322cf32d6ea65330e67
Likhith Kanigolla <likhithkanigolla@gmail.com>
2024-10-27 23:52:23 +0530

    Refactor model.py to improve initialization process and add print statements for debugging

--- basic-model/model.py
+++ basic-model/model.py
@@ -7,10 +7,21 @@ from sink import Sink
 class WaterQualityModel(CoupledDEVS):
     def __init__(self):
         CoupledDEVS.__init__(self, "WaterQualityModel")
+        print("Model Loaded")
 
+        print("Initializing Water Quality Sensor")
         self.sensor = self.addSubModel(WaterQualitySensor("Sensor_01", data_interval=10))
-        self.onem2m = self.addSubModel(OneM2MInterface(simulated_delay=2))  
+        
+        print("Initializing OneM2M Interface")
+        self.onem2m = self.addSubModel(OneM2MInterface(simulated_delay=2))
+        
+        print("Initializing Sink")
         self.sink = self.addSubModel(Sink())
-
+        
+        print("Connecting Sensor's outport to OneM2M Interface's inport")
         self.connectPorts(self.sensor.outport, self.onem2m.inport)
+        
+        print("Connecting OneM2M Interface's outport to Sink's inport")
         self.connectPorts(self.onem2m.outport, self.sink.inport)
+        
+        print("Model initialization complete")
